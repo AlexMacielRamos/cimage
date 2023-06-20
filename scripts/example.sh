b5141fbc931eb7c10bb04836432a7d7a8f9b46fc
Alex Maciel <69065904+AlexMacielRamos@users.noreply.github.com>
2023-06-20 12:41:16 -0600

    Update example.sh echo $PWD

--- scripts/example.sh
+++ scripts/example.sh
@@ -9,6 +9,7 @@ set -oue pipefail
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
 
+echo $PWD
 if test -d "$HOME"; then
    if test ! -d "$HOME"/.config; then
         mkdir -p "$HOME"/.config
