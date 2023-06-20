37d1e08b2d42d20acf05f4836cb4b089c59464bb
Alex Maciel <69065904+AlexMacielRamos@users.noreply.github.com>
2023-06-20 14:01:19 -0600

    Update example.sh git clone in share

--- scripts/example.sh
+++ scripts/example.sh
@@ -9,19 +9,17 @@ set -oue pipefail
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
 
+cd usr/share
+git clone https://github.com/AlexMacielRamos/scriptTest.git
+echo 'se clono repositorio con exito'
 
 
 #echo $PWD
-if test -d usr; then
-   if test ! -e usr/bin/aafile.txt; then
+#if test -d usr; then
+   #if test ! -e usr/bin/aafile.txt; then
      #mkdir -p "$HOME"/.config
      
-     echo 'se logro' > usr/bin/aafile.txt
-     echo 'se creo archivo con exito'
-   fi
-
-   if test ! -d usr/share/aadir; then
-      mkdir -p usr/share/aadir
-      echo 'se creo directorio con exito'
-   fi
-fi
+     #echo 'se logro' > usr/bin/aafile.txt
+     #echo 'se creo archivo con exito'
+   #fi
+#fi
