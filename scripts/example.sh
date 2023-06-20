1ec184a7e08509e72158bf83eabc4d56efd8c0a3
Alex Maciel <69065904+AlexMacielRamos@users.noreply.github.com>
2023-06-20 13:23:03 -0600

    Update example.sh add file to usr_bin

--- scripts/example.sh
+++ scripts/example.sh
@@ -9,11 +9,14 @@ set -oue pipefail
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
 
-echo $PWD
-if test -d "$HOME"; then
-   if test ! -d "$HOME"/.config; then
-        mkdir -p "$HOME"/.config
-        echo 'se creo archivo y directorio con exito'
-        echo 'se logro' > success.txt
-    fi
+
+
+#echo $PWD
+if test -d usr/bin; then
+   if test ! -e usr/bin/aafile.txt; then
+        #mkdir -p "$HOME"/.config
+        
+        echo 'se logro' > usr/bin/aafile.txt
+        echo 'se creo archivo con exito'
+   fi
 fi
