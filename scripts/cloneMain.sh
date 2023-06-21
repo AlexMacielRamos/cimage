5f30046e4c3f053e20dc8020d0a838ae510a332c
Alex Maciel <69065904+AlexMacielRamos@users.noreply.github.com>
2023-06-21 12:42:22 -0600

    Create cloneMain.sh

---
+++ scripts/cloneMain.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'This is an example shell script'
+echo 'Scripts here will run during build if specified in recipe.yml'
+
+cd usr/share
+git clone https://github.com/AlexMacielRamos/scriptTest.git
+echo 'se clono repositorio con exito'
+cd scriptTest
+#git fetch origin
+
+
+#echo $PWD
+#if test -d usr; then
+   #if test ! -e usr/bin/aafile.txt; then
+     #mkdir -p "$HOME"/.config
+     
+     #echo 'se logro' > usr/bin/aafile.txt
+     #echo 'se creo archivo con exito'
+   #fi
+#fi
