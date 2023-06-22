cb9970c892bcbaba832f55e5c0386a7205dbd1fd
Alex Maciel <69065904+AlexMacielRamos@users.noreply.github.com>
2023-06-22 13:04:46 -0600

    Update cloneMain.sh pip test

--- scripts/cloneMain.sh
+++ scripts/cloneMain.sh
@@ -9,11 +9,18 @@ set -oue pipefail
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
 
+
+
 cd usr/share
 git clone https://github.com/AlexMacielRamos/scriptTest.git
 echo 'se clono repositorio con exito'
-cd scriptTest
+#cd scriptTest
 #git fetch origin
+
+pip install pyside6
+pip install virtualenv
+mkdir myvenv
+python -m venv /myvenv/myvenv
 
 
 #echo $PWD
